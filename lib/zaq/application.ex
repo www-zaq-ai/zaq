89cc561accd8eda31a364c1a040c4a2ba190d869
Jad <jad.tarabay1@gmail.com>
2026-05-03 22:18:16 +0300

    chore: fix pr comments

--- lib/zaq/application.ex
+++ lib/zaq/application.ex
@@ -59,6 +59,8 @@ defmodule Zaq.Application do
     case Supervisor.start_link(children, opts) do
       {:ok, _pid} = ok ->
         enqueue_release_badge_check_on_startup()
+        # Forces ValidModalities to load so all modality atoms exist in the VM
+        # before LLMDB.load/0 calls String.to_existing_atom/1 on the snapshot.
         _ = ValidModalities.list()
         LLMDB.load()
         ok
