e1b06e6ff3dfa955ad245383019de59eb3202be7
Jad <jad.tarabay1@gmail.com>
2026-05-19 21:22:53 +0300

    chore: fix migration

--- priv/repo/migrations/20260501000005_create_triggers.exs
+++ priv/repo/migrations/20260501000005_create_triggers.exs
@@ -4,19 +4,31 @@ defmodule Zaq.Repo.Migrations.CreateTriggers do
   def change do
     create table(:triggers, primary_key: false) do
       add :id, :binary_id, primary_key: true
-
-      add :workflow_id, references(:workflows, type: :binary_id, on_delete: :delete_all),
-        null: false
-
-      add :type, :string, null: false
-      add :config, :map, null: false, default: %{}
+      add :event_name, :string, null: false, default: ""
       add :enabled, :boolean, null: false, default: true
 
       timestamps(type: :utc_datetime)
     end
 
-    create index(:triggers, [:workflow_id])
-    create index(:triggers, [:type])
-    create index(:triggers, [:workflow_id, :enabled])
+    create index(:triggers, [:enabled])
+    create index(:triggers, [:event_name])
+
+    create table(:trigger_workflows, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+
+      add :trigger_id, references(:triggers, type: :binary_id, on_delete: :delete_all),
+        null: false
+
+      add :workflow_id, references(:workflows, type: :binary_id, on_delete: :delete_all),
+        null: false
+
+      add :position, :integer, null: false, default: 0
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:trigger_workflows, [:trigger_id])
+    create index(:trigger_workflows, [:workflow_id])
+    create unique_index(:trigger_workflows, [:trigger_id, :workflow_id])
   end
 end
