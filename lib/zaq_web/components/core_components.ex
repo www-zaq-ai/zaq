33b6ef21602210406c5528ba8b20b28bddfd7766
jana914 <jana@fayad.fr>
2026-06-18 17:27:52 +0200

    refactor: (BO) design migrate theme toggle in header

--- lib/zaq_web/components/core_components.ex
+++ lib/zaq_web/components/core_components.ex
@@ -88,31 +88,32 @@ defmodule ZaqWeb.CoreComponents do
   """
   def theme_toggle(assigns) do
     ~H"""
-    <div class="card relative flex flex-row items-center border-2 border-base-300 bg-base-300 rounded-full">
-      <div class="absolute w-1/3 h-full rounded-full border-1 border-base-200 bg-base-100 brightness-200 left-0 [[data-theme=light]_&]:left-1/3 [[data-theme=dark]_&]:left-2/3 transition-[left]" />
-
+    <div class="zaq-toggle-group zaq-toggle-group-pill">
       <button
-        class="flex p-2 cursor-pointer w-1/3"
+        type="button"
+        class="zaq-toggle-segment"
         phx-click={JS.dispatch("phx:set-theme")}
         data-phx-theme="system"
       >
-        <.icon name="hero-computer-desktop-micro" class="size-4 opacity-75 hover:opacity-100" />
+        <.icon name="hero-computer-desktop-micro" class="zaq-icon-sm" />
       </button>
 
       <button
-        class="flex p-2 cursor-pointer w-1/3"
+        type="button"
+        class="zaq-toggle-segment"
         phx-click={JS.dispatch("phx:set-theme")}
         data-phx-theme="light"
       >
-        <.icon name="hero-sun-micro" class="size-4 opacity-75 hover:opacity-100" />
+        <.icon name="hero-sun-micro" class="zaq-icon-sm" />
       </button>
 
       <button
-        class="flex p-2 cursor-pointer w-1/3"
+        type="button"
+        class="zaq-toggle-segment"
         phx-click={JS.dispatch("phx:set-theme")}
         data-phx-theme="dark"
       >
-        <.icon name="hero-moon-micro" class="size-4 opacity-75 hover:opacity-100" />
+        <.icon name="hero-moon-micro" class="zaq-icon-sm" />
       </button>
     </div>
     """
